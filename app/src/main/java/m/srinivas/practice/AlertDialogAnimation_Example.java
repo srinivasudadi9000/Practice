396c18a246a2e8cdc12fc349030fb0bf2caa87cc
Srinivas Dadi <srinivasdadi@schemaxtech.com>
2018-07-21 23:32:27 +0530

    custom dialog box with animation effect

--- app/src/main/java/m/srinivas/practice/AlertDialogAnimation_Example.java
+++ app/src/main/java/m/srinivas/practice/AlertDialogAnimation_Example.java
@@ -1,12 +1,15 @@
 package m.srinivas.practice;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AlertDialogAnimation_Example extends Activity {
@@ -23,9 +26,32 @@ TextView myhellow_txt;
             public void onClick(View v) {
 
                 myhellow_txt.startAnimation(shake);
-                showalert("good",AlertDialogAnimation_Example.this);
+                //showalert("good",AlertDialogAnimation_Example.this);
+                showDialog(AlertDialogAnimation_Example.this,"hello");
             }
         });
+
+    }
+
+
+    public void showDialog(Activity activity, String msg){
+        final Dialog dialog = new Dialog(activity,R.style.PauseDialog);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog);
+
+        TextView text = (TextView) dialog.findViewById(R.id.text_dialog);
+        text.setText(msg);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.btn_dialog);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
 
     }
 
